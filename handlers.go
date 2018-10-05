b4afc26947f8b3d9c7f1ab30bc532ccda601cd3e
Rono <derrickrono@gmail.com>
2018-10-05 16:28:38 +0300

    Added function to create new records into the mongo db

--- handlers.go
+++ handlers.go
@@ -1,38 +1,49 @@
 package main
 
 import (
-"github.com/kataras/iris"
+	"Blogify/models"
+	repo "Blogify/repositories"
+	"log"
+
+	"github.com/kataras/iris"
+	"gopkg.in/mgo.v2/bson"
 )
 
-	
-	func index(ctx iris.Context) {
-        ctx.ViewData("message", "Welcome")
-        ctx.View("index.html") 
-    }	
+func index(ctx iris.Context) {
+	ctx.ViewData("message", "Welcome")
+	ctx.View("index.html")
+}
 
-    func loginUser(ctx iris.Context) {
-    	// Login User 
-    }
+func loginUser(ctx iris.Context) {
+	// Login User
+}
 
-    func fetchPosts(ctx iris.Context) {
-    	// Fetch Posts 
-    }
+func fetchPosts(ctx iris.Context) {
+	// Fetch Posts
 
-    func createPosts(ctx iris.Context) {
-    		/*username := ctx.PostValue("Username")
-    		ctx.ViewData("user", username)
-    		ctx.View("welcome.html") */
-    		ctx.View("create_posts.html")
-    	}
+}
 
-    func updatePosts(ctx iris.Context) {
-    		// Handle Update Blog.
-    	}
+func createPosts(ctx iris.Context) {
+	post := models.Post{}
+	name := ctx.PostValue("name")
+	description := ctx.PostValue("description")
+	post.ID = bson.NewObjectId()
+	post.Name = name
+	post.Description = description
+	log.Println("Post values: ", post.Description)
+	repo.CreateOne(post)
 
-    func deletePosts(ctx iris.Context) {
-    		// Handle Delete Blog.
-    	}	
+	ctx.View("create_posts.html")
+}
 
-    func logoutUser(ctx iris.Context) {
-    	// Logout User 
-    }	
+func updatePosts(ctx iris.Context) {
+	// Handle Update Blog.
+}
+
+func deletePosts(ctx iris.Context) {
+	// Handle Delete Blog.
+}
+
+func logoutUser(ctx iris.Context) {
+	// Logout User
+}
