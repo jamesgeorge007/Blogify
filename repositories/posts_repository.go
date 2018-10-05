31570e09dfdd5bb75d25a013435ceb9c6fb2b763
Rono <derrickrono@gmail.com>
2018-10-05 16:27:32 +0300

    added post repository

---
+++ repositories/posts_repository.go
@@ -0,0 +1,37 @@
+package repositories
+
+import (
+	conn "Blogify/config"
+	"Blogify/models"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+var new_post = models.Post{}
+
+var dbconf = conn.Configurations{}
+var db = dbconf.Connect()
+
+const (
+	COLLECTION = "posts"
+)
+
+//Creates a new post
+func CreateOne(post models.Post) error {
+
+	new_post.ID = bson.NewObjectId()
+	new_post.Name = post.Name
+	new_post.Description = post.Description
+	err := db.C(COLLECTION).Insert(&new_post)
+
+	return err
+}
+
+//Retrieve posts
+
+func GetAll() ([]models.Post, error) {
+	var posts []models.Post
+	err := db.C(COLLECTION).Find(bson.M{}).All(posts)
+
+	return posts, err
+}
